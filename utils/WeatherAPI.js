a97ee46feb52dfe27d0363d6a60723e84b7bb297
tarunaravind01 <123703104+tarunaravind01@users.noreply.github.com>
2024-02-04 19:56:38 -0800

    Add files via upload

---
+++ utils/WeatherAPI.js
@@ -0,0 +1,2 @@
+export const API_KEY = "0118a87318e847ba28e5982d2cd8ad8e";
+
---
+++ utils/weatherCond.js
@@ -0,0 +1,52 @@
+export const weatherConditions = {
+  Rain: {
+    color: "#005BEA",
+    title: "Raining Cats and Dogs",
+    subtitle: "Perfect weather for a stroll, if you’re a fish.",
+    icon: "weather-rainy",
+  },
+  Clear: {
+    color: "#f7b733",
+    title: "Blinding Sunshine",
+    subtitle: "Sun’s out, guns out. And by guns, I mean SPF 50 sunscreen.",
+    icon: "weather-sunny",
+  },
+  Thunderstorm: {
+    color: "#616161",
+    title: "Thor’s Having a Tantrum",
+    subtitle:
+      "Great day for kite flying, if you’re seeking a shocking experience.",
+    icon: "weather-lightning",
+  },
+  Clouds: {
+    color: "#1F1C2C",
+    title: "50 Shades of Grey",
+    subtitle: "But in the sky, and way less exciting.",
+    icon: "weather-cloudy",
+  },
+  Snow: {
+    color: "#00d2ff",
+    title: "Snowpocalypse",
+    subtitle:
+      "Time to “accidentally” get snowed in. Who’s up for a Netflix marathon?",
+    icon: "weather-snowy",
+  },
+  Drizzle: {
+    color: "#076585",
+    title: "Drizzle",
+    subtitle: "Nature’s way of spitting on us, gently.",
+    icon: "weather-hail",
+  },
+  Haze: {
+    color: "#66A6FF",
+    title: "Hazy Lazy Day",
+    subtitle: "When the weather decides to smoke up.",
+    icon: "weather-hail",
+  },
+  Mist: {
+    color: "#3CD3AD",
+    title: "Misty Mystique",
+    subtitle: "It's like living in a low-budget horror movie.",
+    icon: "weather-fog",
+  },
+};
